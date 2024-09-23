df6b227a34ec7947932a1e22363130a45fed238f
DevDevangshu404 <150068112+DevDevangshu404@users.noreply.github.com>
2024-09-23 21:45:37 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,10 @@ $(document).ready(function () {
     document.addEventListener('visibilitychange', function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Devangshu Mazumder";
-            $("#favicon").attr("href", "/assets/images/profile6.png");
+            $("#favicon").attr("href", "/assets/images/logo.gif");
         } else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/profile6.png");
+            $("#favicon").attr("href", "/assets/images/logo.gif");
         }
     });
 
